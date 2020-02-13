2520c3a3f69933db0742693e6f202d382acaa1f6
jcirni <josephcirnigliaro@gmail.com>
2020-02-12 19:13:40 -0500

    django project set up with models

---
+++ inspection_reporter/records/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from inspection_reporter.utils import records_validators
+from inspection_reporter.utils.base_model import BaseModel
+
+class Inspection(BaseModel):
+    date = models.DateField()
+    score = models.IntegerField()
+    comments = models.TextField()
+    restaurant_id = models.ForeignKey(
+                        Restaurant, 
+                        on_delete=models.CASCADE
+                    )
+
+class Restaurant(BaseModel):
+    name = models.TextField()
+    street_address = models.TextField()
+    city = models.TextField()
+    state = models.CharField(max_length=2,validators=[validators.is_valid_state])
+    postal_code = models.CharField(max_length=5,validators=[validators.is_intable])
+    is_current = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+class Violation(BaseModel):
+    is_critical = models.BooleanField()
+    code = models.TextField()
+    description = models.TextField()
+    comments = models.TextField(defualt='', blank=True)
+
+    def __str__(self):
+        return self.code
+
+class RestaurantInspectionViolations(models.Model):
+    restaurant_id = models.ForeignKey(
+                        Restaurant,
+                        on_delete=models.CASCADE,
+                    )
+    violation_id = models.ForeignKey(
+                        Violation,
+                        on_delete=models.CASCADE
+                    )
+    inspection_id = models.ForeignKey(
+                        Inspection,
+                        on_delete=models.CASCADE
+                    )
+    average_score = models.DecimalField(decimal_places=2)
+    average_violations = models.DecimalField(decimal_places=2)
+    
+
+
+
+    
---
+++ inspection_reporter/utils/record_validators.py
@@ -0,0 +1,33 @@
+from rest_framework.serializers import ValidationError
+from inspection_reporter.utils.records_constants import ABBREV_STATE
+
+def is_valid_char(val):
+    """Checks a string for non alphabetic characters."""
+    if val.isalpha():
+        return val
+    else:
+        raise ValidationError(
+            'field must only contain letters, no numbers or special characters'
+        )
+
+def is_valid_state_abbrev(val):
+    """validates state code"""
+    val = val.upper()
+    if val in ABBREV_STATE:
+        return val
+    else:
+        raise ValidationError(
+            'not a valid state abbreviation'
+    )
+    
+def is_valid_zip(val):
+    """assuming 5 digit zip"""
+    if val.isdigit():
+        return val
+    else:
+        raise ValidationError(
+        'should be a simple 5 digit zip code'
+    )
+
+def is_valid_address(val):
+    """tests for valid street name and suffix"""
